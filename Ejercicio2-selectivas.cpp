626e410407da1fb509d83a849d34441b47d53d45
Guts <aamamanica@unjbg.edu.pe>
2024-06-14 09:34:20 -0500

    Subiendo el ejercicio 2 de selectivas

---
+++ Ejercicio2-selectivas.cpp
@@ -0,0 +1,34 @@
+/*
+Ejercicio 2: En un almacén se hace un 20% de descuento a los clientes cuya compra supere los
+S/.1000 ¿Cuál será la cantidad que pagara una persona por su compra?
+*/
+
+#include <iostream> 
+using namespace std; 
+int main(){
+	
+	int compra, precio_descto, precio_normal;
+	
+		cout<<"Digite el monto de su compra: "; cin>>compra; 
+		
+	if(compra<0){
+			cout<<endl<<"El monto ingresado no es valido. "; 
+	}
+	else{
+	
+		precio_descto= compra*(1-0.2); 
+		
+		precio_normal= compra*1; 
+		
+	if(compra>1000){
+		cout<<"\nLe corresponde pagar el monto de: "<<precio_descto<<" soles."<<endl; 
+	}
+	else{
+		cout<<"\nEl monto total a pagar es de: "<<precio_normal<<" soles."<<endl;
+	}
+	}
+	
+	
+	
+	return 0;
+}
