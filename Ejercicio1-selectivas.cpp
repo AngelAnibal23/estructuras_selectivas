9d780f15a955e470426de9b296622dbc54e60e78
Guts <aamamanica@unjbg.edu.pe>
2024-06-17 11:23:16 -0500

    Subiendo el ejercicio 1 de estructuras selectivas

---
+++ Ejercicio1-selectivas.cpp
@@ -0,0 +1,27 @@
+/*
+Ejercicio 1: Determinar si un alumno aprueba o reprueba un curso, sabiendo que aprobara si su
+promedio de tres calificaciones es mayor o igual a 70; reprueba en caso contrario.
+*/
+#include <iostream> 
+using namespace std; 
+
+	int main(){
+		
+		int nota1, nota2, nota3, promedio; 
+		
+		cout<<"Digite su primera calificacion: "; cin>>nota1; 
+		cout<<"Digite su segunda calificacion: "; cin>>nota2; 
+		cout<<"Digite su tercera calificacion: "; cin>>nota3; 
+		
+		promedio=(nota1+nota2+nota3)/3; 
+		
+		if (promedio>=70){
+			cout<<"\nUsted aprobo. ";
+ 	    }
+		else{
+			cout<<"\nUsted desaprobo. "; 
+	    }
+				
+		return 0; 
+	}
+
